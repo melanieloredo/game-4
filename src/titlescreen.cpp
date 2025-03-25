9382e3760f9ccec8f7cc8177bd4ae5675269ff59
melanieloredo <melanie.loredo01@utrgv.edu>
2025-03-25 18:31:06 -0500

    titlescreen file, bg

---
+++ src/titlescreen.cpp
@@ -0,0 +1,9 @@
+
+// Import your sprite items here
+#include "bn_regular_bg_items_title_bg.h" //title bg
+//#include "bn_regular_bg_items_logo.h"
+#include "bn_sprite_ptr.h"
+#include "bn_fixed.h"
+#include "bn_math.h"
+#include "bn_keypad.h"
+#include "bn_core.h"
