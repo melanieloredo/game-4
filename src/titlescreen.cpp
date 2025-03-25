17ea138214229f0672cfb20491283efcdf1c2f82
melanieloredo <melanie.loredo01@utrgv.edu>
2025-03-25 18:56:52 -0500

    logo bg sprite

--- src/titlescreen.cpp
+++ src/titlescreen.cpp
@@ -1,6 +1,7 @@
 
 // Import your sprite items here
 #include "bn_regular_bg_items_title_bg.h" //title bg
+#include "bn_regular_bg_items_logo.h" //logo bg (animated)
 //#include "bn_regular_bg_items_logo.h"
 #include "bn_sprite_ptr.h"
 #include "bn_fixed.h"
