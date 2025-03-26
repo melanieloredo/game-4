b99f677c472a9e592b18248f793308cc07c480d0
melanieloredo <melanie.loredo01@utrgv.edu>
2025-03-25 22:06:40 -0500

    fixed the camera border issue

--- include/camera.h
+++ include/camera.h
@@ -7,8 +7,8 @@
 namespace camera_system {
 
     void update_camera(bn::camera_ptr& camera, const bn::fixed_point& target_position, 
-                       bn::fixed min_x = -128 + 120, bn::fixed max_x = 128 - 120,
-                       bn::fixed min_y = -128 + 80,  bn::fixed max_y = 128 - 80,
+                       bn::fixed min_x = -320 + 240, bn::fixed max_x = 320 - 240, //world dims - screen dims
+                       bn::fixed min_y = -240 + 160,  bn::fixed max_y = 240 - 160,
                        bn::fixed follow_speed = 0.1);
 
 }
