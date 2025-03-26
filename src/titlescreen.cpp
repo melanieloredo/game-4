e1a35a045ff0fbbd99cd360c852eb0f6f69a8024
melanieloredo <melanie.loredo01@utrgv.edu>
2025-03-25 19:04:11 -0500

    mascot lamb sprite

--- src/titlescreen.cpp
+++ src/titlescreen.cpp
@@ -3,6 +3,7 @@
 #include "bn_regular_bg_items_title_bg.h" //title bg
 #include "bn_regular_bg_items_logo.h" //logo bg (animated)
 #include "bn_sprite_items_play_button.h" //play button
+#include "bn_sprite_items_mascot.h" //mascot lamb
 
 #include "bn_sprite_ptr.h"
 #include "bn_fixed.h"
