f1566ceb9ff307130d155013e53d277ae68b420a
melanieloredo <melanie.loredo01@utrgv.edu>
2025-03-25 19:06:41 -0500

    flames sprites

--- src/titlescreen.cpp
+++ src/titlescreen.cpp
@@ -4,6 +4,8 @@
 #include "bn_regular_bg_items_logo.h" //logo bg (animated)
 #include "bn_sprite_items_play_button.h" //play button
 #include "bn_sprite_items_mascot.h" //mascot lamb
+#include "bn_sprite_items_flame1.h" //animated flames
+#include "bn_sprite_items_flame2.h"
 
 #include "bn_sprite_ptr.h"
 #include "bn_fixed.h"
