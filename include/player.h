4d6497d11b0bf67753b4e68533bfcb111a97516a
Jino <jino1249@gmail.com>
2025-04-30 02:10:59 -0500

    Lamb Animation Fixed but... check description
    It is fixed up but when you attack the lamb turns golden it is some issue with the color through the json file but at leas the animation is working for now

--- include/player.h
+++ include/player.h
@@ -6,21 +6,27 @@
 #include "bn_sprite_animate_actions.h"
 #include "bn_optional.h"
 #include "bn_vector.h"
-#include "hitbox.h"
 #include "bn_camera_ptr.h"
+#include "hitbox.h"
 
 class Player {
 public:
     Player(int x, int y, const bn::sprite_item& idle_item, const bn::sprite_item& attack_item);
+
     void update(const bn::vector<Hitbox, 10>& obstacles);
+    void attack();
+    void increase_score();
+
     Hitbox get_hitbox() const;
     Hitbox get_attack_hitbox() const;
-    void increase_score();
-    const bn::sprite_ptr& get_sprite() const { return sprite; }
-    void set_camera(bn::camera_ptr& camera) { sprite.set_camera(camera); }
-    bool is_attacking_now() const { return is_attacking; }
+
+    // 🔓 Public accessors
+    const bn::sprite_ptr& get_sprite() const;
+    bool is_attacking_now() const;
+    void set_camera(bn::camera_ptr& camera);
 
 private:
+    // 🔒 Core state
     bn::sprite_ptr sprite;
     const bn::sprite_item& idle_sprite_item;
     const bn::sprite_item& attack_sprite_item;
@@ -28,7 +34,7 @@ private:
     Hitbox hitbox;
     Hitbox attack_hitbox;
 
-    int last_direction;
+    int last_direction; // 0 = left, 1 = right, 2 = up, 3 = down
     int score;
 
     bool is_attacking;
@@ -36,8 +42,7 @@ private:
 
     bn::optional<bn::sprite_animate_action<4>> animation;
 
-    void attack();
     void update_attack_hitbox();
 };
 
-#endif
+#endif // PLAYER_H
