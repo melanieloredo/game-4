4587010056e3150adf960a641196369fa9bcefa7
lopezkor000 <koriellopez@gmail.com>
2025-05-12 14:54:38 -0500

    random rooms

--- src/main.cpp
+++ src/main.cpp
@@ -3,9 +3,13 @@
 #include "../include/titlescreen.h"
 #include "bn_core.h"
 #include "bn_keypad.h"
+#include <bn_random.h>
 
 int main() {
     bn::core::init();
+
+    bn::random random;
+    int roomNum = random.get_int(2);
 
     while (true) {
         unsigned seed = titlescreen();
@@ -14,8 +18,22 @@ int main() {
             bn::core::update();
         }
 
-        Room1::play_game_scene(seed);
-        Room2::play_game_scene(seed + 1); // transition to Room2
+        bool keepGoing = true;
+        while (keepGoing) {
+            switch (roomNum) {
+                case 0: 
+                    Room1::play_game_scene(seed);
+                    break;
+                case 1:
+                    Room2::play_game_scene(seed + 1);
+                    break;
+                default:
+                    Room1::play_game_scene(seed);
+                    break;
+            }
+            roomNum = random.get_int(2);
+            bn::core::update();
+        }
     }
 
     return 0;
